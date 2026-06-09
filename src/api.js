d7a04d274c20fd1c1a37796ff4cb7202ce3e7142
mond <lm13mond@gmail.com>
2026-06-09 19:07:26 +0700

    fix password string

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzxdbmPziRrOhCCCrujkG7eFkxEMZLiEQkUAxezcTitJ5IejbhvyKGS4U37agte4XQDBw/exec";
 
 export async function api(action, payload = {}) {
+  // ✅ ถ้ามี password ให้ force เป็น string เสมอ
+  if (payload.password !== undefined) {
+    payload.password = String(payload.password);
+  }
+
   const res = await fetch(API_URL, {
     method: "POST",
     headers: { "Content-Type": "text/plain" },
